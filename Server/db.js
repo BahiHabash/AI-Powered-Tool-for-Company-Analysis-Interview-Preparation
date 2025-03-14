dcf7903a8a50b0bc8e0525cedcfc35ca48d4c87a
Bahi Habash <bahi.habash.edu@gmail.com>
2025-03-15 01:03:40 +0200

    Configer DataBase (MongoDB)

--- Server/db.js
+++ Server/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-module.exports = () => () => {
-    mongoose
-        .connect(process.env.DATABASE_URL)
-        .then(() => console.log('✅ DB connected successfully...'));
-}
+const db = process.env.DATABASE_URL.replace('<db_password>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(db)
+    .then(() => console.log('✅ DB connected successfully...'))
+    .catch(() => console.log('❌ DB tailed to connect...'));
--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,10 @@
-require('dotenv').config({ path: './env' });
-// require('./db.js')();
+require('dotenv').config({ path: './.env' });
+require('./db.js');
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}...`);
+    console.log(`🚀 Server is running in ${process.env.NODE_ENV} env on port ${PORT}...`);
 });
