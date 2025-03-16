ebe0e43c27140ad10098457636bbacfda7da6db6
Bahi Habash <bahi.habash.edu@gmail.com>
2025-03-16 04:18:00 +0200

    FIX importing db.js

--- Server/db.js
+++ Server/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE_URL.replace('<DATABASE_PASSWORD>', process.env.DATABASE_PASSWORD);
 
-modeule.exprts = () => {
-    mongoose
-        .connect(DB)
-        .then(() => console.log('✅ DB connected successfully...'))
-        .catch(() => console.log('❌ DB tailed to connect...'));
+module.exports = function () {
+    return function () {
+        mongoose
+            .connect(DB)
+            .then(() => console.log('✅ DB connected successfully...'))
+            .catch(() => console.log('❌ DB tailed to connect...'));
+    }
 }
